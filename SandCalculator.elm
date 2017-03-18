f3e4e4771a6abe489c85de616421a0b4da75a947
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-03-18 15:26:16 -0700

    run elm-format

--- SandCalculator.elm
+++ SandCalculator.elm
@@ -4,69 +4,89 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-type alias Model =
-  { length : String
-  , width : String
-  , depth : String
-  }
 
+type alias Model =
+    { length : String
+    , width : String
+    , depth : String
+    }
+
+
+initialModel : Model
 initialModel =
-  { length = ""
-  , width = ""
-  , depth = ""
-  }
+    { length = ""
+    , width = ""
+    , depth = ""
+    }
+
 
 type Msg
-  = Length String
-  | Width String
-  | Depth String
+    = Length String
+    | Width String
+    | Depth String
 
+
+update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Length length ->
-      { model | length = length }
-    Width width ->
-      { model | width = width }
-    Depth depth ->
-      { model | depth = depth }
+    case msg of
+        Length length ->
+            { model | length = length }
 
+        Width width ->
+            { model | width = width }
+
+        Depth depth ->
+            { model | depth = depth }
+
+
+view : Model -> Html Msg
 view model =
-  div [ ]
-    [ h4 [ ] [ text "How much do I need?"]
-    , input [ type_ "text", onInput Length] [ ]
-    , input [ type_ "text", onInput Width] [ ]
-    , input [ type_ "text", onInput Depth] [ ]
-    , viewResult (calculatePounds model)
-    ]
+    div []
+        [ h4 [] [ text "How much do I need?" ]
+        , input [ type_ "text", onInput Length ] []
+        , input [ type_ "text", onInput Width ] []
+        , input [ type_ "text", onInput Depth ] []
+        , viewResult (calculatePounds model)
+        ]
 
+
+viewResult : Float -> Html Msg
 viewResult pounds =
-  let
-    result =
-      if pounds > 0 then
-        "You need " ++ (toString pounds) ++ " pounds"
-      else
-        ""
-  in
-    h4 [ ] [ text result ]
+    let
+        result =
+            if pounds > 0 then
+                "You need " ++ (toString pounds) ++ " pounds"
+            else
+                ""
+    in
+        h4 [] [ text result ]
 
+
+calculatePounds : Model -> Float
 calculatePounds model =
-  let
-    l =
-      parseFloat model.length
-    w =
-      parseFloat model.width
-    d =
-      parseFloat model.depth
-  in
-    l * w * d
+    let
+        l =
+            parseFloat model.length
 
+        w =
+            parseFloat model.width
+
+        d =
+            parseFloat model.depth
+    in
+        l * w * d
+
+
+parseFloat : String -> Float
 parseFloat string =
-  String.toFloat string
-  |> Result.withDefault 0.0
+    String.toFloat string
+        |> Result.withDefault 0.0
 
+
+main : Program Never Model Msg
 main =
-  Html.beginnerProgram
-    { model = initialModel
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
